6926f64bd269b301a67141ce535bf5bd74b0c1a3
lehcode <53556648+lehcode@users.noreply.github.com>
2025-01-06 04:09:15 +0200

    feat: Legacy login form and controller

--- app/controllers/LoginController.js
+++ app/controllers/LoginController.js
@@ -2,103 +2,106 @@
  * Created by Antony Repin on 02.05.2017.
  */
 
+export const loginControllerFactory = [
+  '$scope',
+  '$rootScope',
+  '$state',
+  '$q',
+  '$http',
+  'settings',
+  '$window',
+  '$location',
+  'localStorageService',
 
-angular.module('AdminApp')
-  .controller('LoginController',
-    [
-      '$scope',
-      '$rootScope',
-      '$state',
-      '$q',
-      '$http',
-      'settings',
-      '$window',
-      '$location',
-      'localStorageService',
+  function (
+    $scope,
+    $rootScope,
+    $state,
+    $q,
+    $http,
+    settings,
+    $window,
+    $location,
+    localStorageService
+  ) {
+    console.log('Initializing LoginController');
 
-      function ($scope,
-                $rootScope,
-                $state,
-                $q,
-                $http,
-                settings,
-                $window,
-                $location,
-                localStorageService) {
+    $scope.$on('$viewContentLoaded', function () {
+      console.info('Login view content loaded');
+    });
 
-        console.log("Initializing LoginController");
+    $scope.user = {};
+    $scope.authUser = null;
 
-        $scope.$on('$viewContentLoaded', function () {
-          console.info("Login view content loaded");
-        });
+    /**
+     * Process user login
+     */
+    $scope.doLogin = function () {
+      if (this.loginForm.$valid) {
+        const req = {
+          method: 'POST',
+          url: settings.apiHost + '/' + $scope.apiRoot + '/authenticate',
+          data: $scope.user,
+          headers: { 'content-type': 'application/json' },
+        };
 
-        $scope.user = {};
-        $scope.authUser = null;
+        $http(req).then(function loginCallback(loginResponse) {
+          if (loginResponse.status === 200) {
+            if (loginResponse.data.status === 'success') {
+              try {
+                localStorageService.set(
+                  'token',
+                  loginResponse.data.data.attributes.token
+                );
+              } catch (error) {
+                console.error(error.message);
+                return;
+              }
 
-        /**
-         * Process user login
-         */
-        $scope.doLogin = function () {
+              Object.assign(req, {
+                method: 'POST',
+                url: settings.apiHost + '/' + $scope.apiRoot + '/user/me',
+                data: null,
+                headers: {
+                  'content-type': 'application/json',
+                  authorization: 'Bearer ' + localStorageService.get('token'),
+                },
+              });
 
-          if (this.loginForm.$valid) {
-            const req = {
-              method: 'POST',
-              url: settings.apiHost + '/' + $scope.apiRoot + '/authenticate',
-              data: $scope.user,
-              headers: {"content-type": "application/json"}
-            };
-
-            $http(req)
-              .then(function loginCallback(loginResponse) {
-
-                if (loginResponse.status === 200) {
-                  if (loginResponse.data.status === 'success') {
-
-                    try {
-                      localStorageService.set('token', loginResponse.data.data.attributes.token);
-                    } catch (error) {
-                      console.error(error.message);
-                      return;
-                    }
-
-                    Object.assign(req, {
-                      method: 'POST',
-                      url: settings.apiHost + '/' + $scope.apiRoot + '/user/me',
-                      data: null,
-                      headers: {
-                        "content-type": "application/json",
-                        "authorization": "Bearer " + localStorageService.get('token')
-                      }
-                    });
-
-                    $http(req)
-                      .then(function userDataCallback(userDataResponse) {
-                        if (userDataResponse.status === 200) {
-                          if (userDataResponse.data.status == 'success') {
-
-                            const userJson = JSON.stringify(userDataResponse.data.data[0]);
-                            localStorageService.set('user', userJson);
-                            $rootScope.setUserData();
-                            $scope.getNavigation();
-                            $state.go('dashboard');
-                          }
-                        }
-                      });
+              $http(req).then(function userDataCallback(userDataResponse) {
+                if (userDataResponse.status === 200) {
+                  if (userDataResponse.data.status == 'success') {
+                    const userJson = JSON.stringify(
+                      userDataResponse.data.data[0]
+                    );
+                    localStorageService.set('user', userJson);
+                    $rootScope.setUserData();
+                    $scope.getNavigation();
+                    $state.go('dashboard');
                   }
                 }
               });
-          } else {
-            this.loginForm.$setDirty();
-            let alerts = '';
-            $rootScope.getFormErrors(this.loginForm)
-              .forEach(function (msg, idx) {
-                $scope.formAlerts.push({type: 'danger', msg: msg});
-                alerts += msg + "<br/>";
-              });
-            console.log($scope.formAlerts);
-            toastr.options.closeButton = true;
-            toastr["error"](alerts, "Login Error");
+            }
           }
-        }
+        });
+      } else {
+        this.loginForm.$setDirty();
+        let alerts = '';
+        $rootScope.getFormErrors(this.loginForm).forEach(function (msg, idx) {
+          $scope.formAlerts.push({ type: 'danger', msg: msg });
+          alerts += msg + '<br/>';
+        });
+        console.log($scope.formAlerts);
+        toastr.options.closeButton = true;
+        toastr['error'](alerts, 'Login Error');
+      }
+    };
+  },
+];
 
-      }]);
+// Register with AngularJS module
+const AdminApp = angular
+  .module('AdminApp')
+  .controller('LoginController', loginControllerFactory);
+
+export default AdminApp;
--- app/services/authService.js
+++ app/services/authService.js
@@ -2,22 +2,46 @@
  * Created by Antony Repin on 8/4/2017.
  */
 
-'use strict';
+export const authServiceFactory = [
+  'settings',
+  '$q',
 
-angular.module('AdminApp')
-  .factory('authService',
-    [
-      'settings',
-      '$q',
+  function (settings, $q) {
+    const service = {};
 
-      function (settings,
-                $q) {
+    service.getAuthData = async function() {
+      const token = localStorageService.get('token');
+      if (!token) {
+        return null;
+      }
 
-        return {
-          getAuthData: function () {
-            return $q(function(resove, reject){});
+      try {
+        const response = await $http({
+          method: 'POST',
+          url: `${settings.apiHost}/${settings.apiRoot}/user/me`,
+          headers: {
+            'content-type': 'application/json',
+            'authorization': `Bearer ${token}`
           }
-        };
+        });
 
-      }]
-  );
+        if (response.status === 200 && response.data.status === 'success') {
+          return response.data.data[0];
+        }
+        return null;
+      } catch (error) {
+        console.error('Auth data fetch error:', error);
+        return null;
+      }
+    };
+
+    return service;
+  },
+];
+
+// Register with AngularJS module
+const AdminApp = angular
+  .module('AdminApp')
+  .factory('authService', authServiceFactory);
+
+export default AdminApp;
